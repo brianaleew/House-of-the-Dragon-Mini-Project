4dd9af19e83ccbb59447d87983413b561478af07
Briana Wright <brianaleew@gmail.com>
2023-01-20 09:03:10 -0500

    changed fonts..struggling to get js buttons to have event listener

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
 console.log('Linked')
+
+// Defining Elements Needed
+const bioBtn = document.querySelectorAll('bio-button')
+const statusBtn = document.querySelectorAll('status-button')
+const bioText = document.getElementById('bio-text')
+const statusText = document.getElementById('status-text')
+
+// let bioBtnArray = [bioBtn]
+// console.log(bioBtnArray)
+
+
+// Clicker Functions for Bio and Status Buttons
+let bioClick = () => {
+    console.log('bio has been clicked')
+}
+
+let statusClick = () => {
+
+}
+
+//Adding Event Listeners
+bioBtn.forEach((item) => {
+    item.addEventListener('click', bioClick)
+});
+
+
+
